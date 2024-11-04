6211b2150e217043c4bc18a9ddc4cad4ea920313
Arvin Ghaffari <arvinghaffari75@gmail.com>
2024-11-04 18:21:59 +0100

    feat: create introduce section

---
+++ src/components/introduce/Introduce.jsx
@@ -0,0 +1,25 @@
+import { HiHome } from "react-icons/hi2";
+import { Description } from "../../ui/description/Description.style";
+import { Container } from "./Introduce.style";
+import Header from "../../ui/header/Header";
+import ResumeButton from "../resume/resume-button/ResumeButton";
+// import { motion } from "framer-motion";
+function Introduce() {
+  return (
+    // <motion.div
+    //   initial={{ opacity: 0 }}
+    //   animate={{ opacity: 1 }}
+    //   transition={{ duration: 1 }}
+    // >
+    <Container>
+      <Header icon={<HiHome />} title="introduce" isLarge={true}>
+        Say Hi from <span>Arvin</span>, Front-End Developer
+      </Header>
+      <Description>This is description</Description>
+      <ResumeButton />
+    </Container>
+    // </motion.div>
+  );
+}
+
+export default Introduce;
---
+++ src/ui/section/Section.jsx
@@ -0,0 +1,7 @@
+import { Container } from "./Section.style";
+
+function Section({ children }) {
+  return <Container>{children}</Container>;
+}
+
+export default Section;
