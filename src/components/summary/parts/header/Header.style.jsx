3cac8519a8f1603b411d856e2932b8afa884e860
Arvin Ghaffari <arvinghaffari75@gmail.com>
2025-01-05 17:51:02 +0100

    refactor : delete comments

--- src/components/summary/parts/header/Header.style.jsx
+++ src/components/summary/parts/header/Header.style.jsx
@@ -6,15 +6,4 @@ const Container = styled.div`
   align-items: center;
 `;
 
-// const Logo = styled(Avatar)`
-//   width: 50px;
-//   height: 50px;
-//   margin-left: 8px;
-// `;
-
-const Role = styled.p`
-  font-size: 1.25rem;
-  font-weight: 800;
-`;
-
-export { Container, Role };
+export { Container };
